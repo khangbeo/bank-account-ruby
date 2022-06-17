d96d5dcd755563eb92b7586790884c1561f532b7
Anthony Duong <83148834+khangbeo@users.noreply.github.com>
2022-06-17 01:43:22 -0700

    Update bank-account.rb

--- bank-account.rb
+++ bank-account.rb
@@ -1,35 +1,37 @@
-class Account
+class Account 
   attr_reader :name
   attr_reader :balance
-  def initialize(name, balance=100)
+  def initialize(name, balance = 100)
     @name = name
     @balance = balance
   end
-  
+
   public
   def display_balance(pin_number)
     if pin_number == @pin
-      puts "Balance: $#{@balance}."
+      puts "Balance: $#{@balance}"
     else
       puts pin_error
     end
   end
-  
-  def withdraw(pin_number,amount)
-    if pin_number == @pin
-      @balance -= amount
-      puts "Withdrew #{amount}."
-    elsif @balance < amount
-      puts "You don't have enough money to withdraw #{amount}. \n Current balance: #{@balance}."
+
+  def withdraw(pin_number, amount)
+    if pin_number == pin
+      if (amount > @balance)
+        puts "You cannot overdraw your account. Balance: #{@balance}, Amount: #{amount}. If you did your balance would be #{@balance - amount}"
+      else
+        @balance -= amount
+        puts "Withdrew #{amount}. New balance: $#{@balance}."     
+      end
     else
       puts pin_error
     end
   end
 
   def deposit(pin_number, amount)
-    if pin_number == @pin
+    if pin_number == pin
       @balance += amount
-      puts "Deposited #{amount}. New Balance: #{@balance}"
+      puts "Deposited #{amount}. New balance: $#{@balance}."
     else
       puts pin_error
     end
@@ -39,13 +41,14 @@ class Account
   def pin
     @pin = 1234
   end
+
   def pin_error
     return "Access denied: incorrect PIN."
-  end 
+  end
 end
 
-checking_account = Account.new("Eric", 1_000_000)
-checking_account.deposit(1234, 1000)
-
+checking_account = Account.new("Anthony", 10000)
+checking_account.withdraw(1234, 100000)
 class CheckingAccount < Account; end
 class SavingsAccount < Account; end
+checking_account.display_balance(1234)
